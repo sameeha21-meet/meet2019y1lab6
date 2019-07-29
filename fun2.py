922f33176c0f8ef8bfee9ddf80a753932debff7f
sameeha21-meet <sameeha21@meet.mit.edu>
2019-07-29 12:31:51 +0300

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,8 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: /home/student/sameeha21_lab6/meet2019y1lab6/lab_6.py =======
+1
+1
+>>> 	
---
+++ lab_6.py
@@ -0,0 +1,14 @@
+def add_numbers(start,end):
+           total=0
+           for number in range(start,end):
+               print(number)
+               total=total+number
+           return total
+           print(total)
+test1= add_numbers(1,2)
+print(test1)
+test2=add_numbers(1,100)
+print(test2)
+test3=add_numbers(1000,5000)
+print(test3)
+       
